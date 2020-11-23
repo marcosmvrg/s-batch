d167fb0999e5ad278e18c67afd0f7438761d7ffc
Marcos Vinicius Rocha Gomes <marcosmvrg@gmail.com>
2020-11-23 16:43:17 -0300

    primeiro batch

---
+++ src/main/java/com/curso/sbatch/SBatchApplication.java
@@ -0,0 +1,13 @@
+package com.curso.sbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBatchApplication.class, args);
+	}
+
+}
