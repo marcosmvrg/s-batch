190a5281c9e366112c03120a0883a4fc9b07c29e
Marcos Vinicius Rocha Gomes <marcosmvrg@gmail.com>
2020-11-24 09:29:35 -0300

    primeiro job
    configurando banco

--- src/main/java/com/curso/sbatch/jobs/FirstJob.java
+++ src/main/java/com/curso/sbatch/jobs/FirstJob.java
@@ -1,4 +1,4 @@
-package com.curso.sbatch.config;
+package com.curso.sbatch.jobs;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -6,16 +6,19 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableBatchProcessing
 @Configuration
-public class BatchConfig {
+public class FirstJob {
 
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
@@ -25,17 +28,30 @@ public class BatchConfig {
 
 	@Bean
 	public Job helloWorld() {
-		return jobBuilderFactory.get("helloWorld").start(printHelloWorld()).build();
+		return jobBuilderFactory
+				.get("helloWorld")
+				.start(printHelloWorld())
+				.incrementer(new RunIdIncrementer()) //incrementa a execucao do job porque o padrão é uma unica execucao. 
+				.build();
 	}
 
 	protected Step printHelloWorld() {
-		return stepBuilderFactory.get("printHelloWorld").tasklet(new Tasklet() {
+		return stepBuilderFactory
+				.get("printHelloWorld")
+				.tasklet(printTasklet(null))
+				.build();
+	}
+
+	@Bean
+	@StepScope
+	protected Tasklet printTasklet(@Value("#{jobParameters['name']}") String name) {
+		return new Tasklet() {
 
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("Hello World!!!!!");
+				System.out.println(String.format("Hello %s!", name));
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		};
 	}
 }
